02ec238f1e014205b30ba28529fc8c8f955251a8
fargusMD <bragaruion@gmail.com>
2020-06-06 00:39:54 +0400

    Updates

---
+++ precache-manifest.faf9ca3228c01137ce167329f28c7b9e.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f14d6c12205b8594fa0e3f7b5e80718",
+    "url": "/food-court/index.html"
+  },
+  {
+    "revision": "e8f296a72b9ba3e1594c",
+    "url": "/food-court/static/css/main.df6a72fe.chunk.css"
+  },
+  {
+    "revision": "dca265e5d5d37971bb77",
+    "url": "/food-court/static/js/2.8bb466ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-court/static/js/2.8bb466ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8f296a72b9ba3e1594c",
+    "url": "/food-court/static/js/main.6bb9e830.chunk.js"
+  },
+  {
+    "revision": "fd0818d0e53170a53447",
+    "url": "/food-court/static/js/runtime-main.ce5321ef.js"
+  },
+  {
+    "revision": "721cee7d0162cf0d24b41762f966da02",
+    "url": "/food-court/static/media/about.721cee7d.jpg"
+  },
+  {
+    "revision": "57e37509971bfa30607cd1f800b7f642",
+    "url": "/food-court/static/media/assorted-salads.57e37509.jpg"
+  },
+  {
+    "revision": "0ffdf9867794ef1da1c145b3f6a35f78",
+    "url": "/food-court/static/media/avocado-cooked.0ffdf986.jpg"
+  },
+  {
+    "revision": "0b697e7a3d69bf9e50055916ad9ca7d6",
+    "url": "/food-court/static/media/blur-breakfast.0b697e7a.jpg"
+  },
+  {
+    "revision": "3819f8bc8603c1e4f592661c36d24a0f",
+    "url": "/food-court/static/media/book-time.3819f8bc.jpg"
+  },
+  {
+    "revision": "6baf8cc70a332963f1e4d74a9f181604",
+    "url": "/food-court/static/media/chef-1.6baf8cc7.jpg"
+  },
+  {
+    "revision": "37f0a96cd779bfeb27aad9b3b4b4b0bc",
+    "url": "/food-court/static/media/chef-hero.37f0a96c.jpg"
+  },
+  {
+    "revision": "623a038df5c1aefeaca7e1ba58c5a7a9",
+    "url": "/food-court/static/media/chocolate.623a038d.jpg"
+  },
+  {
+    "revision": "103c94f4b9d408cf62a9141d8330de03",
+    "url": "/food-court/static/media/dessert.103c94f4.jpg"
+  },
+  {
+    "revision": "0082b7bb44a909849c02de77c4e36e62",
+    "url": "/food-court/static/media/food-dinner.0082b7bb.jpg"
+  },
+  {
+    "revision": "32aeb10a53ed049e29662523da0a64eb",
+    "url": "/food-court/static/media/hero.32aeb10a.jpg"
+  },
+  {
+    "revision": "dc55914e3ba08ac236d1eed991b76444",
+    "url": "/food-court/static/media/logo.dc55914e.png"
+  },
+  {
+    "revision": "2cf7b3ff79add8d8c1a9121ae7dd4412",
+    "url": "/food-court/static/media/pasta.2cf7b3ff.jpg"
+  },
+  {
+    "revision": "d6653c10bcd0f0054752c1371dbcd021",
+    "url": "/food-court/static/media/plate.d6653c10.jpg"
+  },
+  {
+    "revision": "652067c9f6458e6bb154005dd77e61cb",
+    "url": "/food-court/static/media/shrimps-salad.652067c9.jpg"
+  }
+]);
